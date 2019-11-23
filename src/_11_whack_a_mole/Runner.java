c31c7a41343c104db102ccedd3b69314b3d1824d
League Student <league@iMac-21.attlocal.net>
2019-11-22 18:25:51 -0800

    whack a mole 7

---
+++ src/_11_whack_a_mole/Runner.java
@@ -0,0 +1,16 @@
+package _11_whack_a_mole;
+
+public class Runner {
+
+
+
+
+public static void main(String[] args) {
+	Whack_A_Mole runner = new Whack_A_Mole();
+	runner.jbutton();
+}
+
+
+
+
+}
--- src/_11_whack_a_mole/Whack_A_Mole.java
+++ src/_11_whack_a_mole/Whack_A_Mole.java
@@ -1,30 +1,82 @@
 package _11_whack_a_mole;
 
-public class Whack_A_Mole {
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Date;
+import java.util.Random;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+public class Whack_A_Mole implements ActionListener {
 
-	
-	
-	
-	
-	
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	Date date = new Date();
+
+	void jbutton() {
+		frame.dispose();
+		frame = new JFrame();
+		panel = new JPanel();
+		Random ran = new Random();
+		int randnum = ran.nextInt(24);
+
+		frame.add(panel);
+
+		for (int i = 0; i < 24; i++) {
+
+			JButton button = new JButton();
+			panel.add(button);
+			;
+			if (randnum == i) {
+				button.setText("Mole!");
+			}
+			frame.setVisible(true);
+			button.addActionListener(this);
+			frame.setPreferredSize(new Dimension(400, 500));
+			frame.pack();
+		}
+
+	}
+	int whacked = 0;
+	int moleswhacked = 0;
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+
+	whacked++;	
+		JButton pressed = (JButton) e.getSource();
+		if (pressed.getText().equalsIgnoreCase("Mole!")) {
+			jbutton();
+
+			moleswhacked++;
+			
+
+		} else {
+			speak("you missed child");
+		}
+		if (whacked == 10) {
+			endGame(date,moleswhacked);
+			
+		}
+	}
+
+	void speak(String words) {
+		try {
+			Runtime.getRuntime().exec("say " + words).waitFor();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void endGame(Date timeAtStart, int molesWhacked) {
+		Date timeAtEnd = new Date();
+		JOptionPane.showMessageDialog(null, "Your whack rate is "
+				+ ((timeAtEnd.getTime() - timeAtStart.getTime()) / 1000.00 / molesWhacked) + " moles per second.");
+	}
 
 }
